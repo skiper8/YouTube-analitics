293209c3e1a301c8df4d178e21781de8f22050a1
askip <a.skiper2013@yandex.ru>
2023-02-26 03:56:39 +0300

    add content

--- utils/utils.py
+++ utils/utils.py
@@ -4,14 +4,41 @@ from googleapiclient.discovery import build
 
 
 class Channel:
+    api_key: str = os.getenv('API_KEY')
+    youtube = build('youtube', 'v3', developerKey=api_key)
+    channel_id = 'UCMCgOm8GZkHp8zJ6l7_hIuA'
+    channel = youtube.channels().list(id=channel_id, part='snippet,statistics').execute()
+    info_channel = json.dumps(channel, indent=2, ensure_ascii=False)
 
-    def __init__(self, channel_id1):
-        self.channel_id1 = channel_id1  # вДудь
+    def __init__(self, channel_id):
+        self._channel_id = self.channel['items'][0]['id']
+        self.title = self.channel['items'][0]['snippet']['title']
+        self.channel_description = self.channel['items'][0]['snippet']['description']
+        self.channel_url = "https://www.youtube.com/channel/" + channel_id
+        self.num_subscribers = self.channel['items'][0]['statistics']['subscriberCount']
+        self.num_videos = self.channel['items'][0]['statistics']['videoCount']
+        self.num_views = self.channel['items'][0]['statistics']['viewCount']
+
+    @property
+    def channel_id(self):
+        return self._channel_id
 
     def print_info(self):
-        # YT_API_KEY скопирован из гугла и вставлен в переменные окружения
-        api_key: str = os.getenv('API_KEY')
-        # создать специальный объект для работы с API
-        youtube = build('youtube', 'v3', developerKey=api_key)
-        channel = youtube.channels().list(id=self.channel_id1, part='snippet,statistics').execute()
-        print(json.dumps(channel, indent=2, ensure_ascii=False))
+        print(self.info_channel)
+        return json.loads(self.info_channel)
+
+    def get_service(self):
+        return self.youtube
+
+    def save_to_json(self, file_path):
+        channel_info = {
+            "channel_id": self._channel_id,
+            "title": self.title,
+            "channel_description": self.channel_description,
+            "channel_url": self.channel_url,
+            "num_subscribers": self.num_subscribers,
+            "num_videos": self.num_videos,
+            "num_views": self.num_views
+        }
+        with open(file_path, "w", encoding='UTF-8') as file:
+            json.dump(channel_info, file, indent=4)
